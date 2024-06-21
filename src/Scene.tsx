6dc06df13b2e0576fc29951bad58d94461e23bc1
Francesco <francesco.magalini@unitn.it>
2024-06-21 12:27:50 +0200

    Added VR controls

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,18 +1,24 @@
 import { Canvas } from "@react-three/fiber";
-import { Environment, OrbitControls } from "@react-three/drei";
+import { Environment } from "@react-three/drei";
+import { VRButton, XR, Controllers } from "@react-three/xr";
 
 type inputProps = { scene?: "iceland" | "mountain" | "warehouse" };
 
 function Scene({ scene = "iceland" }: inputProps) {
   return (
-    <Canvas>
-      <ambientLight intensity={Math.PI / 2} />
-      <Environment
-        background={true}
-        files={`./environments/${scene}/${scene}.hdr`}
-      />
-      <OrbitControls />
-    </Canvas>
+    <>
+      <VRButton />
+      <Canvas>
+        <XR>
+          <ambientLight intensity={Math.PI / 2} />
+          <Environment
+            background={true}
+            files={`./environments/${scene}/${scene}.hdr`}
+          />
+          <Controllers />
+        </XR>
+      </Canvas>
+    </>
   );
 }
 
